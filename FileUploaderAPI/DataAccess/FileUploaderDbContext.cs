e9352b589a43f28990027e0f6685064e85b00651
Muhammed Saleem P <muhammedsalimp@gmail.com>
2022-01-01 22:38:55 +0530

    Add files via upload

---
+++ FileUploaderAPI/DataAccess/FileUploaderDbContext.cs
@@ -0,0 +1,16 @@
+﻿using FileUploaderAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FileUploaderAPI.DataAccess
+{
+    public class FileUploaderDbContext : DbContext
+    {
+        public FileUploaderDbContext(DbContextOptions<FileUploaderDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UploadURLToken> UploadURLTokens { get; set; }
+        public DbSet<UploadedFileMetadata> UploadedFileMetadata { get; set;}
+    }
+}
---
+++ FileUploaderAPI/Models/UploadURLToken.cs
@@ -0,0 +1,9 @@
+﻿namespace FileUploaderAPI.Models
+{
+    public class UploadURLToken
+    {
+        public int Id { get; set; }
+        public string Token { get; set; }
+        public bool IsExpired { get; set; }
+    }
+}
